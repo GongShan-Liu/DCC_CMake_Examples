70f4f74fa6bf5e550d7c1cf4bc8384734f9697c5
GongShan-Liu <38199105+GongShan-Liu@users.noreply.github.com>
2025-09-27 15:47:01 +0800

    Update MainMayaCMake.bat

--- CMake_Maya/MainMayaCMake.bat
+++ CMake_Maya/MainMayaCMake.bat
@@ -23,10 +23,10 @@ set maya_sdk_path="C:/Program Files/Autodesk/Maya_2020_4_devkit"
 @REM set maya_sdk_path=""
 
 @REM @REM 构建文件
-"D:\Program Files\CMake\bin\cmake.exe" -DMAYA_SDK_LOCATION=%maya_sdk_path% -DMAYA_VERSION=%compile_maya_version% -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE --no-warn-unused-cli -S %cd% -B %cd%/build/%compile_maya_version% -G "Visual Studio 15 2017" -T host=x64 -A x64 -DCMAKE_CONFIGURATION_TYPES=Release
+"C:\Program Files\CMake\bin\cmake.exe" -DMAYA_SDK_LOCATION=%maya_sdk_path% -DMAYA_VERSION=%compile_maya_version% -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE --no-warn-unused-cli -S %cd% -B %cd%/build/%compile_maya_version% -G "Visual Studio 15 2017" -T host=x64 -A x64 -DCMAKE_CONFIGURATION_TYPES=Release
 
 @REM 调试或发布 --config Debug; --config Release
-"D:\Program Files\CMake\bin\cmake.exe" --build %cd%/build/%compile_maya_version% --config Release --target ALL_BUILD
+"C:\Program Files\CMake\bin\cmake.exe" --build %cd%/build/%compile_maya_version% --config Release --target ALL_BUILD
 
 
 @REM 检查maya的命令端口是否打开是否存在 如果打开就执行测试代码，否则就打开maya
